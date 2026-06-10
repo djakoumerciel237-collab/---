ebf08f4890e52c3c6f2dd4c4ec7b2a29aa97f9bc
djakoumerciel237-collab <djakoumerciel237@gmail.com>
2026-06-11 00:16:40 +0100

    Fix name assignment in goodbye command

--- cmds/goodbye.js
+++ cmds/goodbye.js
@@ -2,7 +2,7 @@ export default {
     name: "goodbye",
     run: async (api, event, args, config) => {
         if(event.logMessageType === "log:unsubscribe") {
-            const name = event.logMessageData.leftParticipantName || "Quelqu'un";
+            const name = event.logMessageData.leftParticipantName;
             api.sendMessage(`Bye ${name} 👋`, event.threadID);
         }
     }
